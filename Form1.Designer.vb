d8e415576c56e53efa8f71cbe0eb04ccf2ffb78f
Jesse <boatengjesse921@gmail.com>
2025-06-28 02:30:06 +0000

    Initial commit - added VB project with .gitignore

---
+++ Form1.Designer.vb
@@ -0,0 +1,74 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btnGo = New Button()
+        btnVariables = New Button()
+        btnDataTypes = New Button()
+        SuspendLayout()
+        ' 
+        ' btnGo
+        ' 
+        btnGo.Location = New Point(74, 66)
+        btnGo.Name = "btnGo"
+        btnGo.Size = New Size(275, 158)
+        btnGo.TabIndex = 0
+        btnGo.Text = "Go"
+        btnGo.UseVisualStyleBackColor = True
+        ' 
+        ' btnVariables
+        ' 
+        btnVariables.Location = New Point(427, 76)
+        btnVariables.Name = "btnVariables"
+        btnVariables.Size = New Size(269, 138)
+        btnVariables.TabIndex = 1
+        btnVariables.Text = "Variable Demonstration"
+        btnVariables.UseVisualStyleBackColor = True
+        ' 
+        ' btnDataTypes
+        ' 
+        btnDataTypes.Location = New Point(74, 271)
+        btnDataTypes.Name = "btnDataTypes"
+        btnDataTypes.Size = New Size(275, 153)
+        btnDataTypes.TabIndex = 2
+        btnDataTypes.Text = "Data Types"
+        btnDataTypes.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(btnDataTypes)
+        Controls.Add(btnVariables)
+        Controls.Add(btnGo)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents btnGo As Button
+    Friend WithEvents btnVariables As Button
+    Friend WithEvents btnDataTypes As Button
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,45 @@
+﻿Imports System.Runtime.CompilerServices
+
+Public Class Form1
+    Private Sub btnGo_Click(sender As Object, e As EventArgs) Handles btnGo.Click
+
+        MessageBox.Show("This is another message")
+
+    End Sub
+
+    Private Sub btnVariables_Click(sender As Object, e As EventArgs) Handles btnVariables.Click
+
+        Dim stFirstName As String
+        Dim stLastName As String
+
+        stFirstName = "Jesse"
+        stLastName = "Boateng"
+
+        MessageBox.Show("hello and welcome " & stFirstName & " " & stLastName & " I hope you are well")
+
+    End Sub
+
+    Private Sub btnDataTypes_Click(sender As Object, e As EventArgs) Handles btnDataTypes.Click
+        Dim stMake As String
+        Dim stModel As String
+        Dim isDoors As Integer
+        Dim stColour As String
+        Dim bTaxed As Boolean
+        Dim iEngineSize As Integer
+        Dim decPrice As Decimal
+        Dim dtDateRegistered As Date
+
+        stMake = "Ford"
+        stModel = "Benz"
+        isDoors = 15
+        stColour = "black"
+        bTaxed = True
+        iEngineSize = 1200
+        decPrice = 999.99
+        dtDateRegistered = #11/2/2020#
+
+        MessageBox.Show("These cars are " & stMake & " and " & stModel & "." & vbNewLine &
+                        isDoors & " doors. ")
+
+    End Sub
+End Class
